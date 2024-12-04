9587aa1e4fcc317362ed6e090fcb02781880a1d5
Ana Carolina <anacarolinassouzasantos@gmail.com>
2024-12-04 14:54:13 -0300

    Automação Auth

---
+++ cypress/e2e/update-booking.cy.js
@@ -0,0 +1,68 @@
+/// <reference types="cypress" />
+
+describe("Update Booking", () => {
+  var token = "";
+  var bookingId = "";
+
+  before("Login", () => {
+    cy.request({
+      method: "POST",
+      url: "https://restful-booker.herokuapp.com/auth",
+      body: {
+        username: "admin",
+        password: "password123",
+      },
+    }).then((response) => {
+      token = response.body.token;
+    });
+  });
+
+  beforeEach("Create a booking", () => {
+    cy.request({
+      method: "POST",
+      url: "https://restful-booker.herokuapp.com/booking",
+      failOnStatusCode: false,
+      body: {
+        firstname: "Ana",
+        lastname: "Teste",
+        totalprice: 5000,
+        depositpaid: true,
+        bookingdates: {
+          checkin: "2024-12-01",
+          checkout: "2024-12-15",
+        },
+        additionalneeds: "Breakfast",
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body).to.be.a("number");
+      bookingId = response.body.bookingId;
+    });
+  });
+  it("Update a booking", () => {
+    cy.request({
+      method: "PUT",
+      url: `https://restful-booker.herokuapp.com/booking/${bookingId}`,
+      failOnStatusCode: false,
+      body: {
+        firstname: "Ana",
+        lastname: "Teste",
+        totalprice: 5000,
+        depositpaid: true,
+        bookingdates: {
+          checkin: "2024-12-01",
+          checkout: "2024-12-15",
+        },
+        additionalneeds: "Breakfast",
+      },
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        Cookie: `token=${token}`,
+      },
+    }).then((response) => {
+      expect(response.status).to.equal(200);
+      expect(response.body.totalprice).to.equal(5000);
+    });
+  });
+});
